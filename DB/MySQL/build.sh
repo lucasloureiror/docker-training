75e5206713839b9071d77d61c0e3469d3261c68f
Lucas <prolucasloureiro@gmail.com>
2023-01-24 14:02:50 -0300

    fix: fixed bug in dockerfile

--- DB/MySQL/build.sh
+++ DB/MySQL/build.sh
@@ -1,5 +1,1 @@
-docker build -t mysql .
-
-docker run mysql -p 3030:3306
-
-docker run mysql -v ./docker/data/mysql:/var/lib/mysql --env=MYSQL_ROOT_PASSWORD=root --env="MYSQL_PASSWORD=root" --env="MYSQL_DATABASE=test"
+docker compose up
